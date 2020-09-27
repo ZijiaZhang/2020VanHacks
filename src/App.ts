b4c60041e9bd98a30a43492ca90a8b2823006503
Zijia Zhang <Zijia.Zhang99@gmail.com>
2020-09-26 22:11:47 -0700

    Add configurable port

--- src/App.ts
+++ src/App.ts
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 mongoose.connect('mongodb://localhost:27017/2020Vanhacks', {useNewUrlParser: true,  useUnifiedTopology: true, useCreateIndex: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -16,5 +16,5 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/v1', apiRouterV1);
 
-app.listen(3000, ()=> console.log(`Example app listening on 3000`));
+app.listen(port, ()=> console.log(`Example app listening on ${port}`));
 
